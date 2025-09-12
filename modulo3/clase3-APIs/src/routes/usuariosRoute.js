c2e1eb826dc981b90deb66420ed82d2d5d9e5b80
Joaquin-Allue <Alluejoaquin@hotmail.com>
2025-09-12 10:52:57 -0300

    Base de los usuarios2

--- modulo3/clase3-APIs/src/routes/usuariosRoute.js
+++ modulo3/clase3-APIs/src/routes/usuariosRoute.js
@@ -1,13 +1,23 @@
-export class RepositorioDeUsuarios {
-    constructor() {
-        this.usuarios = [];
+import express from 'express';
+import { RepositorioDeUsuarios } from '../repositories/usuariosRepository.js';
+
+const router = express.Router();
+const repo = new RepositorioDeUsuarios();
+
+router.post("/", (req, res) => {
+    const email = req.body.email;
+
+    const usuariosConMismoEmail = repo.buscarPorEmail(email);
+
+    if(usuariosConMismoEmail.length > 0) {
+        res.status(400).json({
+            success: false,
+            message: "Ya existe un usuario con ese email."
+        });
+        return;
     }
 
-    guardar(usuario) {
-        this.usuarios.push(usuario);
-    }
+    const usuario = new Usuario();
 
-    buscarPorEmail(email) {
-        return this.usuarios.filter(usuario => usuario.email === email);
-    }
-}
+    repo.guardar(usuario);
+});
